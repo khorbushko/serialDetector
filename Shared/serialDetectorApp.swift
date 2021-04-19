9aa01f20fda5b21514d4921f7731240c3637537b
Kyryl <kirill.ge@gmail.com>
2021-04-19 21:49:09 +0300

    Initial Commit

---
+++ Shared/serialDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  serialDetectorApp.swift
+//  Shared
+//
+//  Created by Kyryl Horbushko on 19.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct serialDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
